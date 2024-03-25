a96df3d298a2825ad99c6150e5565f40f8a38676
sasha0552 <admin@sasha0552.org>
2024-03-25 21:21:34 +0000

    Refactor

--- nvidia_pstate/__init__.py
+++ nvidia_pstate/__init__.py
@@ -1,1 +1,1 @@
-from nvidia_pstate.pstate import set_pstate, set_pstate_low, set_pstate_high
+from .pstate import set_pstate, set_pstate_low, set_pstate_high
--- nvidia_pstate/scripts/nvidia_pstate.py
+++ nvidia_pstate/scripts/nvidia_pstate.py
@@ -1,34 +1,41 @@
 import argparse
+import importlib.metadata
 
-from nvidia_pstate import set_pstate
+import nvidia_pstate
 
 
-def list_command(args):
+# Function to list available performance states
+def query_pstate(args):
   # TODO
   print("Does not work for now")
 
-# Function to set performance state
-def set_command(args):
-  set_pstate(args.id, args.performance_state)
+# Function to specify performance state
+def set_pstate(args):
+  nvidia_pstate.set_pstate(args.id, args.perf_state)
+
 
 def main():
+  # Determine program version
+  try:
+    __version__ = importlib.metadata.version("nvidia-pstate")
+  except:
+    __version__ = "unknown"
+
   # Create the main argument parser with a description
-  parser = argparse.ArgumentParser(description="CLI utility for managing performance states of NVIDIA GPUs.")
+  parser = argparse.ArgumentParser(description="CLI utility for managing performance states of NVIDIA GPUs")
 
-  # Create subparsers for different commands
-  subparsers = parser.add_subparsers(title="Commands", dest="command", required=True)
+  parser.add_argument("-i", "--id", type=int, nargs="+", help="target a specific GPU")
+  parser.add_argument("-ps", "--perf-state", type=int, help="specify performance state")
+  parser.add_argument("-q", "--query", action="store_true", help="list available performance states")
+  parser.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
 
-  # "list" command parser
-  list_parser = subparsers.add_parser("list-pstates", help="List available performance states of GPU")
-  list_parser.add_argument("-i", "--id", type=int, nargs="+", help="Target a specific GPU")
-  list_parser.set_defaults(func=list_command)
-
-  # "set" command parser
-  set_parser = subparsers.add_parser("set-pstate", help="Set performance state for GPU")
-  set_parser.add_argument("-i", "--id", type=int, nargs="+", help="Target a specific GPU")
-  set_parser.add_argument("-ps", "--performance-state", type=int, required=True, help="Specifies performance state.")
-  set_parser.set_defaults(func=set_command)
-
-  # Parse command-line arguments and execute the appropriate function
+  # Parse command-line arguments
   args = parser.parse_args()
-  args.func(args)
+
+  # Execute the appropriate function
+  if args.perf_state is not None:
+    set_pstate(args)
+  elif args.query is not None:
+    query_pstate(args)
+  else:
+    parser.print_help()
