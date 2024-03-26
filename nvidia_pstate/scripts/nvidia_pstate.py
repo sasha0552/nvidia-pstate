39db1d18e6c42af8988ad0866e0d606be98d00ab
sasha0552 <admin@sasha0552.org>
2024-03-26 03:20:30 +0000

    Add option to suppress output

--- nvidia_pstate/scripts/nvidia_pstate.py
+++ nvidia_pstate/scripts/nvidia_pstate.py
@@ -11,7 +11,7 @@ def query_pstate(args):
 
 # Function to specify performance state
 def set_pstate(args):
-  nvidia_pstate.set_pstate(args.id, args.perf_state)
+  nvidia_pstate.set_pstate(args.id, args.perf_state, silent=args.silent)
 
 
 def main():
@@ -27,6 +27,7 @@ def main():
   parser.add_argument("-i", "--id", type=int, nargs="+", help="target a specific GPU")
   parser.add_argument("-ps", "--perf-state", type=int, help="specify performance state")
   parser.add_argument("-q", "--query", action="store_true", help="list available performance states")
+  parser.add_argument("-s", "--silent", action="store_true", help="suppress output messages")
   parser.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
 
   # Parse command-line arguments
@@ -35,7 +36,7 @@ def main():
   # Execute the appropriate function
   if args.perf_state is not None:
     set_pstate(args)
-  elif args.query is not None:
+  elif args.query is not None and args.query:
     query_pstate(args)
   else:
     parser.print_help()
